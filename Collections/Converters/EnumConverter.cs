bcd9834c01ad264dfbc705c07fe070d0d6e3d098
Michael L Perry <michael@qedcode.com>
2014-09-17 21:12:11 -0500

    Initial sample project.

---
+++ Collections/Converters/EnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Collections.Converters
+{
+    public class EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ChangeType(value, targetType, CultureInfo.CurrentCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.ToObject(targetType, (byte)(int)value);
+        }
+    }
+}
